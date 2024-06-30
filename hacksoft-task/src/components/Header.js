4f552b6c090160a473de6f08e8351f24d0684f4e
IvanGoranoff <ivangoranoff@gmail.com>
2024-06-30 13:51:20 +0300

    not a fix

--- hacksoft-task/src/components/Header.js
+++ hacksoft-task/src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import useStickyHeader from '../hooks/useStickyHeader';
 import '../styles/HeaderStyles.css';
 import logo from '../assets/hacksoftlogo.svg';
+import avatar from '../assets/avatar.png';
 
-function Header() {
+
+const Header = () => {
+    const sticky = useStickyHeader();
+
     return (
-        <div className="header">
+        <header className={`header ${sticky ? 'sticky' : ''}`}>
             <img src={logo} alt="HackSoft Logo" className="logo" />
             <div className="profile-icon">
-                <img src="../assets/avatar.png" alt="User Profile" />
+                <img src={avatar} alt="Profile" />
             </div>
-        </div>
+        </header>
     );
 }
 
